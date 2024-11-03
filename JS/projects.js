44ac851375f1d8cd0019edc78453e9297de1c437
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2024-11-03 20:28:59 +0400

    Adding new projects

--- JS/projects.js
+++ JS/projects.js
@@ -1,21 +1,33 @@
 function createProjectHTML(project) {
   return `
-    <div class="project">
+     <div class="project">
       <img src=".${project.image}" alt="title" />
       <h3>${project.title}</h3>
       <h4>Tech Stack: ${project.techStack}</h4>
       <p>${project.description}</p>
       <div class="project-btns">
-        <a href="${project.sourceCode}" target="_blank">
-          <button class="project-url btn">
-            <i class="fa-brands fa-github"></i> Src
-          </button>
-        </a>
-        <a href="${project.viewDemo}" target="_blank">
-          <button class="project-url btn">
-            <i class="fa-solid fa-display"></i> View
-          </button>
-        </a>
+        ${
+          project.sourceCode
+            ? `
+          <a href="${project.sourceCode}" target="_blank">
+            <button class="project-url btn">
+              <i class="fa-brands fa-github"></i> Src
+            </button>
+          </a>
+        `
+            : ""
+        }
+        ${
+          project.viewDemo
+            ? `
+          <a href="${project.viewDemo}" target="_blank">
+            <button class="project-url btn">
+              <i class="fa-solid fa-display"></i> View
+            </button>
+          </a>
+        `
+            : ""
+        }
       </div>
     </div>
   `;
