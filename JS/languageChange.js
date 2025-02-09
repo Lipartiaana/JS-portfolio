a64a6ef36538a3dbbf79c61c33fa99109a822cd0
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2025-02-09 19:13:36 +0400

    Updating about-page content

--- JS/languageChange.js
+++ JS/languageChange.js
@@ -66,9 +66,18 @@ function updatePageContent(lang) {
       profession: "Frontend Developer",
       cv: "Download CV",
       aboutTitle: "ABOUT ME",
-      aboutMe: `<p> I am a hardworking, passionate woman web developer, with great problem-solving skills, committed to breaking barriers in the world of technology. I am proud to have actively participated in the '500 Women in Tech' program, which provided me with the opportunity to broaden my horizons.</p> 
-      <p> Over the past 5 months, I've had the privilege of working as a front-end developer intern at "Unilab", where I've been a part of exciting projects and continued to expand my technical skills.</p>
-      <p> If you are seeking a dedicated and enthusiastic team player, I'm here to explore new opportunities and challenges with you.</p>`,
+      aboutMe: `<p> Hello, I am Ana — a hardworking and detail-oriented web developer with strong problem-solving skills. My goal is to break down barriers for women in the world of technology.</p> 
+      <p> My interest in technology began in childhood because my family members were employed in the information technology field. However, I never considered becoming a programmer as my future profession. While studying at the Faculty of Psychology at <span> "Tbilisi State University"</span>, I realized that my relationship with technology was not just a hobby and that I needed to seriously try myself in this field. </p> 
+      <p> I started learning through the free stages of <span>"Bitcamp's"</span> Front-End Development program, which captivated me. After that, I began studying independently on YouTube and Udemy. </p>
+      <p> <i class="fa-solid fa-graduation-cap"></i> I was accepted into numerous funded programs: </p> 
+      <ul> <li> - <span>"BTU" and "USAID" </span> - "500 Women in Tech". </li>  
+      <li> - <span>"Harvard University"</span> - CS50x - Introduction to computer science" </li> 
+      <li> - <span>"IsSoft"</span> - Front-End Development School" </li>
+      <li> - <span>"Alte University"</span> - "Angular in Depth" </li> </ul>
+      <h4> Work Experience </h4>
+      <hr>
+      <p> <i class="fa-solid fa-suitcase"></i> <span> Internship </span>- I first encountered real projects at Ilia University's Cyber Laboratory <span> "Unilab," </span> where I joined as an intern-developer. I was involved in interesting projects and gained experience working with a team, which helped me improve not only my technical skills but also my soft skills. </p> 
+      <p> <i class="fa-solid fa-briefcase"></i> <span> Freelance </span> - Over the past year, I have actively worked on freelance projects, including collaborations with organizations such as <span> "Cherie" </span> coffee, <span> "Abasha Municipality"</span>, and the non-governmental organization  <span> "New Idea." </span> </p> `,
       contactTitle: "Contact me:",
       contactSend: "Send",
       projectsTitle: "My projects",
@@ -81,9 +90,18 @@ function updatePageContent(lang) {
       profession: "Frontend დეველოპერი",
       cv: "ჩამოტვირთე CV",
       aboutTitle: "ჩემ შესახებ",
-      aboutMe: `<p> მე ვარ შრომისმოყვარე, ტექნოლოგიებით გატაცებული ქალი ვებ დეველოპერი, პრობლემების გადაჭრის უნარით, მსურს გავარღვიო ბარიერები ტექნოლოგიების სამყაროში. ვამაყობ, რომ აქტიურად ვიღებდი მონაწილეობას პროგრამაში "500 ქალი ტექში", რამაც მომცა შესაძლებლობა გამეფართოებინა ჩემი შესაძლებლობები. </p>
-      <p> ბოლო 5 თვის განმავლობაში მე მქონდა პრივილეგია მემუშავა "უნილაბში" სტაჟიორ - დეველოპერად, სადაც ვიყავი საინტერესო პროექტების ნაწილი ეს ყველაფერი კი დამეხმარა ჩემი ტექნიკური უნარების გაფართოებაში. </p> 
-      <p> თუ თქვენ ეძებთ მონდომებულ და ენთუზიაზმით სავსე გუნდის წევრს, მზად ვარ თქვენთან ერთად ახალი შესაძლებლობებისა და გამოწვევების მისაღებად.</p> `,
+      aboutMe: `<p> გამარჯობა, მე ვარ ანა — შრომისმოყვარე და დეტალებზე ორიენტირებული ვებ დეველოპერი, პრობლემების გადაჭრის უნარით, ჩემი მიზანია ტექნოლოგიების სამყაროში ქალებისთვის ბარიერების დამსხვრევა.</p>
+      <p> ჩემი დაინტერესება ტექნოლოგიებით ბავშვობიდან დაიწყო, რადგან ჩემი ოჯახის წევრები საინფორმაციო ტექნოლოგიების სფეროში იყვნენ დასაქმებულები, თუმცა ჩემს მომავალ პროფესიად პროგრამისტობას არ განვიხილავდი. <span>"თბილისის სახელმწიფო უნივერსიტეტი" </span>- ს ფსიქოლოგიის ფაკულტეტზე სწავლის დროს მივხვდი, რომ ტექნოლოგიებთან ჩემი ურთიერთობა მხოლოდ გატაცება არ იყო და სერიოზულად უნდა მეცადა ამ სფეროში თავი. </p> 
+      <p> სწავლა <span>'Bitcamp'</span>- ის Front-end დეველოპმენტის უფასო ეტაპებით დავიწე, რამაც ძალიან გამიტაცა, ამის შემდეგ დავიწყე Youtube-იდან და Udemy -დან სწავლა დამოუკიდებლად.</p>
+      <p> <i class="fa-solid fa-graduation-cap"></i> მოვხვდი უამრავ დაფინანსებულ პროგრამაში, მათ შორის: </p>
+      <ul> <li>  - <span>"BTU"</span> და <span>"USAID"</span> - "500 ქალი ტექში" </li> 
+      <li> - <span>"Harvard University"</span> - CS50x - შესავალი კომპიუტერულ მეცნიერებებში</li> 
+      <li> - <span> 'IsSoft'</span>Frontend development school </li>
+      <li> - <span>"ალტე უნივერსიტეტი" </span> - "Angular in Depth"</ul></p>
+      <h4>სამუშაო გამოცდილება</h4>
+      <hr>
+      <p> <i class="fa-solid fa-suitcase"></i> <span> სტაჟირება </span> - რეალურ პროექტებთან შეხება პირველად ილიას უნივერსიტეტის კიბერლაბორატორია <span> "უნილაბში"</span> მომეცა, სადაც მოვხვდი სტაჟიორ - დეველოპერად, ჩართული ვიყავი საინტერესო პროექტებში და გამოცდილება მივიღე გუნდთან მუშაობაშიც. რაც დამეხმარა არა მხოლოდ ტექნიკური არამედ Soft Skill-ების დახვეწაშიც.</p> 
+      <p> <i class="fa-solid fa-briefcase"></i> <span> ფრილანსინგი </span> - ბოლო 1 წელია აქტიურად ვმუშაობ Freelance პროექტებზე, მათ შორის ისეთ ორგანიზაციებთან, როგორებიცაა: ყავა <span> "ჩერი" </span>, <span> "აბაშის მუნიციპალიტეტის მერია" </span> და არასამთავრობო ორგანიზაცია <span> "New Idea" </span>. </p> `,
       contactTitle: "კონტაქტი",
       contactSend: "გაგზავნა",
       projectsTitle: "ჩემი პროექტები",
